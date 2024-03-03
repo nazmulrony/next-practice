f8344f9a851d09d7f4eab19618657392baed552e
nazmulrony <nhrony65@gmail.com>
2024-03-03 19:00:41 +0600

    page content shown in modal using parallel routing and route intercepting

--- src/app/photo-gallery/@modal/(..)photo-gallery/[id]/page.tsx
+++ src/app/photo-gallery/@modal/(..)photo-gallery/[id]/page.tsx
@@ -6,10 +6,10 @@ export default function PhotoDetails({ params: { id } }: { params: { id: string 
 	const photoInfo = gallery.find((item) => item.id.toString() == id);
 	return (
 		<Modal>
-			<p className="text-center text-2xl font-bold mb-8">{photoInfo?.name}</p>
+			<p className="text-center  text-white text-2xl font-bold mb-8">{photoInfo?.name}</p>
 			<Image src={photoInfo?.photo as StaticImageData} alt="" className="" />
-			<p className="font-bold mt-4">Location:{photoInfo?.location}</p>
-			<p className="font-bold">Photograph by:{photoInfo?.photographer}</p>
+			<p className="font-bold text-white mt-4">Location:{photoInfo?.location}</p>
+			<p className="font-bold text-white">Photograph by:{photoInfo?.photographer}</p>
 		</Modal>
 	);
 }
