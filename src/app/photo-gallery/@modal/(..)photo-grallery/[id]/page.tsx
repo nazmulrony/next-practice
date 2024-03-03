5b62f818867a0db4b47cb01712c37f4006ae36c1
nazmulrony <nhrony65@gmail.com>
2024-03-03 18:53:26 +0600

    modal added

--- src/app/photo-gallery/@modal/(..)photo-grallery/[id]/page.tsx
+++ src/app/photo-gallery/@modal/(..)photo-grallery/[id]/page.tsx
@@ -1,3 +1,15 @@
-export default function PhotoDetails() {
-	return <div>Hello</div>;
+import { gallery } from '@/app/photo-gallery/gallery-data';
+import Modal from '@/components/ui/modal';
+import Image, { StaticImageData } from 'next/image';
+
+export default function PhotoDetails({ params: { id } }: { params: { id: string } }) {
+	const photoInfo = gallery.find((item) => item.id.toString() == id);
+	return (
+		<Modal>
+			<p className="text-center text-2xl font-bold mb-8">{photoInfo?.name}</p>
+			<Image src={photoInfo?.photo as StaticImageData} alt="" className="" />
+			<p className="font-bold mt-4">Location:{photoInfo?.location}</p>
+			<p className="font-bold">Photograph by:{photoInfo?.photographer}</p>
+		</Modal>
+	);
 }
