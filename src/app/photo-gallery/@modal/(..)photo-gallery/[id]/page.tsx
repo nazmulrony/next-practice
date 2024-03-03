19cec86811ed76317948a7e2fac750c1f2204f99
nazmulrony <nhrony65@gmail.com>
2024-03-03 19:03:22 +0600

    modal  content modified

--- src/app/photo-gallery/@modal/(..)photo-gallery/[id]/page.tsx
+++ src/app/photo-gallery/@modal/(..)photo-gallery/[id]/page.tsx
@@ -6,10 +6,12 @@ export default function PhotoDetails({ params: { id } }: { params: { id: string 
 	const photoInfo = gallery.find((item) => item.id.toString() == id);
 	return (
 		<Modal>
-			<p className="text-center  text-white text-2xl font-bold mb-8">{photoInfo?.name}</p>
-			<Image src={photoInfo?.photo as StaticImageData} alt="" className="" />
-			<p className="font-bold text-white mt-4">Location:{photoInfo?.location}</p>
-			<p className="font-bold text-white">Photograph by:{photoInfo?.photographer}</p>
+			<div className="mx-auto rounded bg-white p-6 ">
+				<p className="text-center text-2xl font-bold mb-8">{photoInfo?.name}</p>
+				<Image src={photoInfo?.photo as StaticImageData} alt="" className="" />
+				<p className="font-bold mt-4">Location:{photoInfo?.location}</p>
+				<p className="font-bold">Photograph by:{photoInfo?.photographer}</p>
+			</div>
 		</Modal>
 	);
 }
